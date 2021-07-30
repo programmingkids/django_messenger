5915e85b53f2383f7695bc51e8fd7cec894f4aa5
EC2 Default User <hiroshi.829f@gmail.com>
2021-07-30 02:47:30 +0000

    complete6

---
+++ messenger/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import Group
+from .models import Message
+
+
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['name']
+        widgets = {
+            'name' : forms.TextInput(attrs={
+                'autofocus' : True,
+            }),
+        }
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['body']
+        widgets = {
+            'body' : forms.Textarea(attrs={
+                'autofocus' : True,
+                'rows' : 3,
+            }),
+        }
---
+++ user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = "user"
+
+urlpatterns = [
+    path('user_detail/<int:pk>', views.UserDetailView.as_view(), name='user_detail'),
+    path('user_update/<int:pk>', views.UserUpdateView.as_view(), name='user_update'),
+    path('user_list/', views.UserListView.as_view(), name='user_list'),
+]
